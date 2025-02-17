ab7ceba5e1d780fd94bfdc2ac90c9c7c5756585b
manuel <nevadofabianmanuel@gmail.com>
2025-02-17 14:14:37 +0100

    feat: loop music

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from playsound import playsound
+from pygame import mixer
 
 
 st.title('Guerrilla Gardens')
@@ -9,11 +10,13 @@ st.title('Guerrilla Gardens')
 am_button = st.button('Am')
 bdim_button = st.button('B dim')
 
+mixer.init()
 
 if am_button:
-    while not bdim_button:
-        playsound('fart.mp3')
+    mixer.music.load('fart.mp3')
+    mixer.music.play(loops=-1)
+
 
 if bdim_button:
-    while not am_button:
-        playsound('wait.mp3')
+    mixer.music.load('wait.mp3')
+    mixer.music.play(loops=-1)
