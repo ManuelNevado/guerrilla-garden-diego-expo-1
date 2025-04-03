9ae5429dcb2f8e01f64c5d09995b5a222add4fdd
ManuelNevado <nevadofabianmanuel@gmail.com>
2025-04-03 15:15:27 +0200

    feat: buttons ok

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import streamlit as st
 from pygame import mixer
 
+# Custom CSS for bigger buttons
+st.markdown("""
+<style>
+    .stButton > button {
+        width: 180px;
+        height: 45px;
+        font-size: 16px;
+        margin: 5px;
+        padding: 8px;
+    }
+    div.row-widget.stButton {
+        text-align: center;
+        margin: 8px 0;
+    }
+</style>
+""", unsafe_allow_html=True)
 
 st.title('////////////////////')
 
-am_button = st.button('Am9')
-bdim_button = st.button('Bdim7')
-bb_button = st.button('Bb7#11')
-cmaj_button = st.button('Cmaj9')
-dhalfdim_button = st.button('Dhalf-dim')
-eb_button = st.button('E7b13')
-fmaj_button = st.button('Fmaj7#11')
-fmmaj7_button = st.button('FmMaj7')
-gb_button = st.button('G7b5b13')
-stop_button = st.button('STOP')
+# Create three columns for better button layout
+col1, col2, col3 = st.columns(3)
 
+# Distribute buttons across columns
+with col1:
+    am_button = st.button('Am9')
+    cmaj_button = st.button('Cmaj9')
+    fmaj_button = st.button('Fmaj7#11')
+
+with col2:
+    bdim_button = st.button('Bdim7')
+    dhalfdim_button = st.button('Dhalf-dim')
+    fmmaj7_button = st.button('FmMaj7')
+
+with col3:
+    bb_button = st.button('Bb7#11')
+    eb_button = st.button('E7b13')
+    gb_button = st.button('G7b5b13')
+
+# Center the stop button
+_, center_col, _ = st.columns([1,1,1])
+with center_col:
+    stop_button = st.button('STOP')
 
 mixer.init()
 
